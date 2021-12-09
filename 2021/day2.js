bb8c9eeb27d05a9119bf23742c34a1d803cdf7eb
James Kovacs <james.kovacs@gm.com>
2021-12-09 15:47:18 -0500

    Adding day 2 of 2021

--- 2021/day2.js
+++ 2021/day2.js
@@ -13,5 +13,57 @@ var DAY = 2;
 })(DAY);
 
 function solve(input) {
+  var position1 = {
+    x: 0,
+    y: 0
+  }, position2 = {
+    x: 0,
+    y: 0,
+    aim: 0
+  };
 
+  for(var i = 0; i < input.length; i++) {
+    calculateMovePart1(input[i].split(' '), position1);
+  }
+
+  console.log('Answer 1', position1.x * position1.y);
+
+  for(var i = 0; i < input.length; i++) {
+    calculateMovePart2(input[i].split(' '), position2);
+  }
+
+  console.log('Answer 2', position2.x * position2.y);
+
+  function calculateMovePart1(move, position) {
+    switch(move[0]) {
+      case 'forward':
+        position.x += parseInt(move[1]);
+        break;
+      case 'up':
+        position.y -= parseInt(move[1]);
+        break;
+      case 'down':
+        position.y += parseInt(move[1])
+        break;
+      default:
+        break;
+    }
+  }
+
+  function calculateMovePart2(move, position) {
+    switch(move[0]) {
+      case 'forward':
+        position.x += parseInt(move[1]);
+        position.y += position.aim * parseInt(move[1]);
+        break;
+      case 'up':
+        position.aim -= parseInt(move[1]);
+        break;
+      case 'down':
+        position.aim += parseInt(move[1])
+        break;
+      default:
+        break;
+    }
+  }
 }
